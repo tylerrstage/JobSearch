1d36a39699c130fcac0685b801e779d11dbb1baa
tylerrstage <tyler.stageberg@gmail.com>
2026-06-29 13:36:01 -0500

    Created an example JobCard as well as DummyData example.

---
+++ job-portal/src/JobDummyData.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id:1,
+        postedOn: '2026-06-29',
+        title: 'Frontend Developer',
+        company: 'Tesla',
+        type: 'Full Time',
+        experience: 'Junior',
+        location: 'On-site',
+        skills: ["JavaScript", "React", "Node.js"],
+        job_link: 'https://www.tesla.com/careers'
+    }
+]
--- job-portal/src/components/JobCard/index.jsx
+++ job-portal/src/components/JobCard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import "tailwindcss";
 
 function JobCard() {
     const skills = ["JavaScript", "React", "Node.js"];
@@ -8,19 +9,22 @@ function JobCard() {
   return (
     <div className='mx-40 mb-4'>
         <div className='flex justify-between items-center bg-zinc-200 rounded-md px-6 py-4 rounded-md border 
-                        border-black shadow-lg hover:border-blue-500 hover:shadow-blue-500 hover:translate-y-1 hover: scale-103'>
-            <div>
-                <h1>Frontend Developer - Amazon</h1>
+                        border-black shadow-lg hover:border-blue-400 hover:shadow-xl'>
+            <div className='flex flex-col items-start gap-3'>
+                <h1 className='text-lg font-semibold'>Frontend Developer - Amazon</h1>
                 <p>Full Time &#x2022; Internship &#x2022; On-site</p>
-                <div>
-                    {skills.map((skill) => {
-                        <p>(skill)</p>
-                    })}
+                <div className='flex items-center gap-2'>
+                    {skills.map((skill) => (
+                        <p key={skill} className='text-gray-500 py-1 px-2 rounded-md border border-black'>{skill}</p>
+                    ))}
                 </div>
             </div>
-            <div>
-                <p>Posted {diffInDays} days ago</p>
-                <button>Apply</button>
+            <div className='flex items-center gap-4'>
+                <p className='text-gray-500'>Posted {diffInDays} days ago</p>
+                <a href="">
+                    <button className='text-blue-500 border border-blue-500 px-10 py-2 rounded-md cursor-pointer 
+                    hover:bg-blue-500 hover:text-white'>Apply</button>
+                </a>
             </div>
         </div>
     </div>
