38203a40f00c6f77a35dad2e62af97f5f89bf87e
tylerrstage <tyler.stageberg@gmail.com>
2026-06-29 22:58:32 -0500

    Made changes to filter and search system.

--- job-portal/src/App.jsx
+++ job-portal/src/App.jsx
@@ -3,15 +3,66 @@ import Header from './components/Header'
 import Searchbar from './components/Searchbar'
 import JobCard from './components/JobCard'
 import JobData from './JobDummyData'
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+import { db } from './firebase.config.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [customSearch, setCustomSearch] = useState(false);
+
+  const fetchJobs = async() => {
+    setCustomSearch(false);
+    const tempJobs =[]
+    const jobsRef = query(collection(db, "jobs"));
+    const q = query(jobsRef, orderBy("postedOn", "desc"));
+    const req = await getDocs(q);
+    req.forEach((job) => {
+      // console.log(doc.id, " => ", doc.data());
+      tempJobs.push({
+        ...job.data(),
+        id: job.id,
+        postedOn: job.data().postedOn.toDate()
+      });
+    });
+    setJobs(tempJobs);
+  }
+
+  const fetchJobsCustom = async(jobCriteria) => {
+    setCustomSearch(true);
+    const tempJobs =[]
+    const jobsRef = query(collection(db, "jobs"));
+    const q = query(jobsRef, where("type", "==", jobCriteria.type), where("title", "==", jobCriteria.title), 
+    where("location", "==", jobCriteria.location), where("experience", "==", jobCriteria.experience), 
+    orderBy("postedOn", "desc"));
+    const req = await getDocs(q);
+
+    req.forEach((job) => {
+      // console.log(doc.id, " => ", doc.data());
+      tempJobs.push({
+        ...job.data(),
+        id: job.id,
+        postedOn: job.data().postedOn.toDate()
+      });
+    });
+    setJobs(tempJobs);
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
 
   return (
     <div>
       <Navbar />
       <Header />
-      <Searchbar />
-      {JobData.map((job) => (
+      <Searchbar fetchJobsCustom={fetchJobsCustom} />
+      {customSearch && 
+      <button onClick={fetchJobs} className='flex mb-2 hover:cursor-pointer'>
+        <p className="bg-blue-500 text-white py-2 px-10 rounded-md">Clear Filters</p>
+      </button>}
+      {jobs.map((job) => (
         <JobCard key={job.id} {...job}/>
       ))}
     </div>
--- job-portal/src/components/Searchbar/index.jsx
+++ job-portal/src/components/Searchbar/index.jsx
@@ -1,37 +1,56 @@
 import React from 'react'
+import { useState } from 'react'
 
-function Searchbar() {
+function Searchbar(props) {
+    const [jobCriteria, setJobCriteria] = useState({
+        title: "",
+        location: "",
+        experience: "",
+        type: ""
+    })
+
+    const handleChange = (e) => {
+        setJobCriteria((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const search = async() => {
+        await props.fetchJobsCustom(jobCriteria);
+    }
+
   return (
     <div className='flex gap-4 my-10 justify-center px-10'>
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
+        <select onChange={handleChange} name="title" value={jobCriteria.title} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
             <option value="" disabled hidden selected>Job Role</option>
             <option value="Frontend Developer">Frontend Developer</option>
             <option value="Backend Developer">Backend Developer</option>
             <option value="Full Stack Developer">Full Stack Developer</option>
         </select>
 
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
+        <select onChange={handleChange} name="type" value={jobCriteria.type} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
             <option value="" disabled hidden selected>Job Type</option>
             <option value="Full Time">Full Time</option>
             <option value="Part Time">Part Time</option>
             <option value="Contract">Contract</option>
         </select>
 
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
+        <select onChange={handleChange} name="location" value={jobCriteria.location} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
             <option value="" disabled hidden selected>Location</option>
             <option value="Remote">Remote</option>
             <option value="On-site">On-site</option>
             <option value="Hybrid">Hybrid</option>
         </select>
 
-        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
+        <select onChange={handleChange} name="experience" value={jobCriteria.experience} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md cursor-pointer'>
             <option value="" disabled hidden selected>Experience</option>
             <option value="Intern">Intern</option>
             <option value="Junior">Junior</option>
             <option value="Senior">Senior</option>
         </select>
 
-        <button className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md hover:bg-blue-600 cursor-pointer'>Search</button>
+        <button onClick={search} className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md hover:bg-blue-600 cursor-pointer'>Search</button>
     </div>
   )
 }
